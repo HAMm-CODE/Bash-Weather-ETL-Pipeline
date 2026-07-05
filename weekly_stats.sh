e92b5bc547693713c520a8113e7a19aefabb3598
Hamm-code <sentsdrimma123@gmail.com>
2026-07-05 15:06:21 +0300

    find minimum and maximum absolute forecast errors for the week

--- weekly_stats.sh
+++ weekly_stats.sh
@@ -20,3 +20,20 @@ for i in {0..6}; do
   # validate result:
   echo ${week_fc[$i]}
 done
+
+#find minimum and maximum values
+minimum=${week_fc[1]}
+maximum=${week_fc[1]}
+for item in ${week_fc[@]}; do
+   if [[ $minimum > $item ]]
+   then
+     minimum=$item
+   fi
+   if [[ $maximum < $item ]]
+   then
+     maximum=$item
+   fi
+done
+
+echo "minimum ebsolute error = $minimum"
+echo "maximum absolute error = $maximum"
