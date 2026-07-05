0531cc3518cba13899ae3b1153e24f0306c0e7f7
Hamm-code <sentsdrimma123@gmail.com>
2026-07-05 14:57:50 +0300

    Load the historical accuracies into an array covering the last week of data

---
+++ weekly_stats.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load the historical accuracies into an array covering the last week of data
+
+echo $(tail -7 synthetic_historical_fc_accuracy.tsv  | cut -f6) > scratch.txt
+
+week_fc=($(echo $(cat scratch.txt)))
+
+# validate result:
+for i in {0..6}; do
+    echo ${week_fc[$i]}
+done
