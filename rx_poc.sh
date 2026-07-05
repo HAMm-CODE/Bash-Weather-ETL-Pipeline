65a369bd96aff6347ff9c05ce90bbc82f50ece3b
Hamm-code <sentsdrimma123@gmail.com>
2026-07-05 13:22:04 +0300

    tomorrow's temperature

--- rx_poc.sh
+++ rx_poc.sh
@@ -12,3 +12,10 @@ curl -s wttr.in/$city?T --output weather_report
 #in one line 
 obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
 echo "The current Temperature of $city: $obs_temp"
+
+# To extract the forecast tempearature for noon tomorrow
+fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
+echo "The forecasted temperature for noon tomorrow for $city : $fc_temp C"
+
+#The first line uses a combination of head and tail to narrow down to the line containing tomorrow's noon forecast temperature. grep and cut are employed to isolate and format 
+#the temperature information, and, the numeric part of the temperature is captured using grep -Eo.
